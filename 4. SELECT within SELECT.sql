122112b797617f67121d5e390de41427b4019ab5
Cole <colehartman00@gmail.com>
2024-07-26 14:36:40 -0700

    progress

---
+++ 4. SELECT within SELECT.sql
@@ -0,0 +1,18 @@
+-- 1.
+-- List each country name where the population is larger than that of 'Russia'.
+
+-- 2.
+-- Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+-- Per Capita GDP
+
+-- 3.
+-- List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+
+-- 4.
+-- Which country has a population that is more than United Kingdom but less than Germany? Show the name and the population.
+
+-- 5.
+-- Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+
+-- 6.
+--
